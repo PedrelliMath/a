c67a57b54ab8a3c7f0d828241e51f450b0829b1f
Matheus Mauricio <lhr3@hotmail.com>
2026-01-21 20:06:35 -0300

    fix: fix typo

--- src/app/config.py
+++ src/app/config.py
@@ -61,7 +61,7 @@ class Settings(BaseSettings):
     jwt_algorithm: str = Field(default="RS256")
     jwt_audience: str
     jwt_scopes: str = Field(default="openid")
-    keycloak_port: int
+    keycloak_server_url: str
     
     @property
     def database(self) -> PostgresSettings:
@@ -84,8 +84,8 @@ class Settings(BaseSettings):
     @property
     def auth(self) -> AuthenticationSettings:
         return AuthenticationSettings(
-            jwks_uri=f"{self.protocol}://{self.app_host}:{self.keycloak_port}/{self.jwks_uri}",
-            issuer=f"{self.protocol}://{self.app_host}:{self.keycloak_port}/{self.issuer}",
+            jwks_uri=f"{self.protocol}://{self.keycloak_server_url}/{self.jwks_uri}",
+            issuer=f"{self.protocol}://{self.keycloak_server_url}/{self.issuer}",
             jwt_algorithm=self.jwt_algorithm,
             jwt_audience=self.jwt_audience,
             jwt_scopes=self.jwt_scopes.split()
